2cf8982685c862fe01520cbfd5b519490a2dd5b5
leon8 <octarine_core@outlook.com>
2017-05-02 14:32:07 +0800

    neural-cam: you need to modifydetector.c for your own usage

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hxhx/darknet/neural-cam/src/main.cpp" "/home/hxhx/darknet/neural-cam/build/CMakeFiles/main.dir/src/main.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "CUDNN"
+  "GPU"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hxhx/darknet/neural-cam/build/CMakeFiles/darknetlib.dir/DependInfo.cmake"
+  "/home/hxhx/darknet/neural-cam/build/CMakeFiles/supportlib.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/local/cuda-8.0/include"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
